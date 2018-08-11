bdd4a2225588cdfdeb33885ac05a1d1e2f5e516a
pierrealbaret <pierre.albaret@gmail.com>
2018-08-11 17:27:35 +0200

    POC: mobile react-native application

---
+++ index.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import MainApp from './src/MainApp';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => MainApp);
